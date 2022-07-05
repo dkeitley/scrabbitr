dd5ddf216c739e4477cab2a913d5c3456a62c2c1
Daniel Keitley <dkeitley@gmail.com>
2022-07-05 11:01:00 +0100

    Added additional colour palettes.

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -14,7 +14,7 @@ celltype_colours <- c(
                       "Anterior Primitive Streak" = "#C19F70",
                       "Node"="#153B3D",
                       "Notochord" = "#0F4A9C",
-                      "Gut tube" = "#EF5A9D",
+                      "Gut tube" = "#F063A4",
                       "Hindgut" = "#F397C0",
                       "Midgut"  = "#FF00B2",
                       "Foregut" = "#FFB7FF",
@@ -52,11 +52,11 @@ celltype_colours <- c(
                       "Haematoendothelial progenitors" = "#FBBE92",
                       "Blood progenitors" = "#6C4B4C",
                       "Erythroid" = "#C72228",
-                      "Erythroid/Masked"="red",
-                      "MEP"="#EF4E22",
+                      "Erythroid/Masked"="#FF0000",
+                      "MEP"="#F15B32",
                       "EMP"="#7C2A47",
                       "YS endothelium"="#FF891C",
-                      "Mesothelium-endothelium/Masked"="blue",#"#907b66",
+                      "Mesothelium-endothelium/Masked"="#0000FF",
                       "Allantois endothelium"="#2F4A60",
                       "Embryo proper endothelium"="#90E3BF",
                       "Venous endothelium"="#BD3400",
@@ -123,7 +123,150 @@ celltype_colours <- c(
 )
 
 
+stage_colours <- c(
+  "E6.5" = "#723957",
+  "E6.75" = "#823C3C",
+  "E7.0" ="#93502D",
+  "E7.25"="#A9742E",
+  "E7.5"="#C3A34B",
+  "E7.75"="#D5CE81",
+  "E8.0"="#CBE0B1",
+  "E8.25"="#A4D8CA",
+  "E8.5"="#73BACC",
+  "E8.75"="#5394BF",
+  "E9.0"="#506BA4",
+  "E9.25"="#61497D",
+  "E9.5"="#723959",
+
+  #Rabbit
+  "GD7" = "#93502D",
+  "GD8" = "#CBE0B1",
+  "GD9" = "#506BA4"
+
+)
+
+
+godsnot_102 <- c(
+  "#FFFF00",
+  "#1CE6FF",
+  "#FF34FF",
+  "#FF4A46",
+  "#008941",
+  "#006FA6",
+  "#A30059",
+  "#FFDBE5",
+  "#7A4900",
+  "#0000A6",
+  "#63FFAC",
+  "#B79762",
+  "#004D43",
+  "#8FB0FF",
+  "#997D87",
+  "#5A0007",
+  "#809693",
+  "#6A3A4C",
+  "#1B4400",
+  "#4FC601",
+  "#3B5DFF",
+  "#4A3B53",
+  "#FF2F80",
+  "#61615A",
+  "#BA0900",
+  "#6B7900",
+  "#00C2A0",
+  "#FFAA92",
+  "#FF90C9",
+  "#B903AA",
+  "#D16100",
+  "#DDEFFF",
+  "#000035",
+  "#7B4F4B",
+  "#A1C299",
+  "#300018",
+  "#0AA6D8",
+  "#013349",
+  "#00846F",
+  "#372101",
+  "#FFB500",
+  "#C2FFED",
+  "#A079BF",
+  "#CC0744",
+  "#C0B9B2",
+  "#C2FF99",
+  "#001E09",
+  "#00489C",
+  "#6F0062",
+  "#0CBD66",
+  "#EEC3FF",
+  "#456D75",
+  "#B77B68",
+  "#7A87A1",
+  "#788D66",
+  "#885578",
+  "#FAD09F",
+  "#FF8A9A",
+  "#D157A0",
+  "#BEC459",
+  "#456648",
+  "#0086ED",
+  "#886F4C",
+  "#34362D",
+  "#B4A8BD",
+  "#00A6AA",
+  "#452C2C",
+  "#636375",
+  "#A3C8C9",
+  "#FF913F",
+  "#938A81",
+  "#575329",
+  "#00FECF",
+  "#B05B6F",
+  "#8CD0FF",
+  "#3B9700",
+  "#04F757",
+  "#C8A1A1",
+  "#1E6E00",
+  "#7900D7",
+  "#A77500",
+  "#6367A9",
+  "#A05837",
+  "#6B002C",
+  "#772600",
+  "#D790FF",
+  "#9B9700",
+  "#549E79",
+  "#FFF69F",
+  "#201625",
+  "#72418F",
+  "#BC23FF",
+  "#99ADC0",
+  "#3A2465",
+  "#922329",
+  "#5B4534",
+  "#FDE8DC",
+  "#404E55",
+  "#0089A3",
+  "#CB7E98",
+  "#A4E804",
+  "#324E72")
+
+
 #' @export
 getCelltypeColours <- function() {
   return(celltype_colours)
+}
+
+#' @export
+getStageColours <- function() {
+  return(stage_colours)
+}
+
+#' @export
+getDiscreteColours <- function(x) {
+  if(class(x)=="int") return(godsnot_102[1:x])
+  else if (length(x)>1) {
+    pal <- godsnot_102[1:length(x)]
+    names(pal) <- names(x)
+    return(pal)
+  }
 }
